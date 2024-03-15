df6ea973afbc700854d3462941ab429b96f6321f
Tim <tim103@pm.me>
2024-03-15 15:43:14 +1100

    fix mobile style

--- .website/layouts/default.vue
+++ .website/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-3xl px-4 py-10 m-auto bg-white sm:px-8 dark:bg-gray-800">
     <main class="max-w-none prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900 hover:prose-a:text-primary-400 prose-a:font-medium prose-a:no-underline prose-a:border-dashed prose-a:border-b hover:prose-a:border-solid hover:prose-a:border-primary-400">
-      <div class="mb-10 flex gap-2 justify-between">
+      <div class="mb-10 flex gap-2 justify-between flex-wrap">
         <div class="flex gap-2">
           <NuxtLink to="/">Home</NuxtLink>
           <NuxtLink to="/#guide">Guide</NuxtLink>
