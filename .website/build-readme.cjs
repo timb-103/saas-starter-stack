9b5883f1051204c82b5e4909d042c6fa1e5092dc
Cameron Kemshal-Bell <cam@panr.app>
2024-03-17 22:40:48 +1100

    Removed comments and added the link in contents for the contribution guide

--- .website/build-readme.cjs
+++ .website/build-readme.cjs
@@ -1,84 +1,4 @@
-// const fs = require('fs');
-// const path = require('path');
-// const matter = require('gray-matter');
-//
-// // Directories where the markdown files are stored
-// const toolsDir = path.join(__dirname, '../tools');
-// const guidesDir = path.join(__dirname, '../guide');
-// const interviewsDir = path.join(__dirname, '../interviews');
-//
-// // Read the content of the header file
-// const headerPath = path.join(__dirname, '../partials/header.md');
-// const headerContent = fs.readFileSync(headerPath, 'utf-8');
-//
-// // Start the content with the header
-// let content = headerContent;
-//
-// // Function to read the content of each file and extract the frontmatter
-// const processFiles = (dir, prependFile) => {
-//     const files = fs.readdirSync(dir);
-//     let items = [];
-//
-//     // If there's a file to prepend, add its content first
-//     if (prependFile) {
-//         const prependFilePath = path.join(__dirname, '../partials', prependFile);
-//         const prependFileContent = fs.readFileSync(prependFilePath, 'utf-8');
-//         content += prependFileContent + '\n';
-//     }
-//
-//     for (const file of files) {
-//         if (path.extname(file) === '.md') {
-//             const filePath = path.join(dir, file);
-//             const fileContent = fs.readFileSync(filePath, 'utf-8');
-//             const parsedContent = matter(fileContent);
-//             items.push({
-//                 name: file.replace('.md', ''),
-//                 content: parsedContent.content,
-//                 category: parsedContent.data.category
-//             });
-//         }
-//     }
-//
-//     // Sort the items by category
-//     if (dir === toolsDir) {
-//         items.sort((a, b) => a.category.localeCompare(b.category));
-//
-//         // Add the items to the content
-//         let currentCategory = '';
-//         for (const item of items) {
-//             // If the category has changed, add a new header
-//             if (item.category !== currentCategory) {
-//                 currentCategory = item.category;
-//                 content += '### ' + currentCategory + '\n';
-//             }
-//             content += '- [' + item.name + '](#' + item.name + ')\n';
-//             content += item.content + '\n';
-//         }
-//     } else {
-//         addItemsToContent(items);
-//     }
-// }
-//
-// // Function to add the items to the content
-// const addItemsToContent = (items) => {
-//     for (const item of items) {
-//         content += '### ' + item.name + '\n';
-//         content += item.content + '\n';
-//     }
-// }
-//
-// // Process the files in each directory
-// processFiles(guidesDir, 'guide.md');
-// processFiles(interviewsDir, 'interviews.md');
-// processFiles(toolsDir, 'tools.md');
-//
-// // Add the contribution section
-// const contributePath = path.join(__dirname, '../partials/contribute.md');
-// const contributeContent = fs.readFileSync(contributePath, 'utf-8');
-// content += contributeContent;
-//
-// // Write the combined content to the README file
-// fs.writeFileSync(path.join(__dirname, '../README.md'), content);
+// This script is used to build the README file by combining the header, sections, and contribute files with the items files.
 
 const fs = require('fs');
 const path = require('path');
@@ -102,6 +22,8 @@ for (const section of sections) {
         }
     }
 }
+// Add the contribute link to the table of contents
+content += '- [Contribute](#contribute)\n';
 content += '\n';
 
 // Add the sections to the content
@@ -160,10 +82,7 @@ for (const section of sections) {
         content += '- [' + item.name + '](' + item.link + ') - ' + item.description + '\n';
     }
 
-    // // Add the items to the content
-    // for (const item of items) {
-    //     content += '- [' + item.name + '](' + item.link + ') - ' + item.description + '\n';
-    // }
+    content += '\n';
 
 }
 
